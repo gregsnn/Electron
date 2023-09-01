190ae8e88bbc91a826bdbf83e02bf0fbac916618
Gregori Nunes <gregorisnunes@outlook.com>
2023-09-01 10:57:52 -0300

    adicionando projeto electron-react

---
+++ electron-react/main.js
@@ -0,0 +1,100 @@
+const { app, BrowserWindow, ipcMain, Notification, Menu, Tray } = require('electron');
+const path = require('path');
+const isDev = !app.isPackaged;
+const isMac = process.platform === 'darwin';
+
+const dockIcon = path.join(__dirname, 'assets', 'images', 'react_app_logo.png');
+const trayIcon = path.join(__dirname, 'assets', 'images', 'react_icon.png');
+
+// function createSplashWindow() {
+//   const win = new BrowserWindow({
+//     width: 400,
+//     height: 200,
+//     frame: false,
+//     transparent: true,
+//     webPreferences: {
+//       nodeIntegration: false,
+//       worldSafeExecuteJavaScript: true,
+//       contextIsolation: true,
+//     }
+//   })
+
+//   win.loadFile('splash.html')
+//   return win;
+// }
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    backgroundColor: '#fff',
+    show: false,
+    webPreferences: {
+      nodeIntegration: false,
+      worldSafeExecuteJavaScript: true,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+
+  if (isDev) win.webContents.openDevTools();
+  return win;
+}
+
+if (isDev) {
+  require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+  })
+}
+
+if (isMac) {
+  app.dock.setIcon(dockIcon);
+}
+
+let tray = null;
+app.whenReady()
+  .then(() => {
+    // const template = require('./utils/Menu').createTemplate(app);
+    // const menu = Menu.buildFromTemplate(template);
+    // Menu.setApplicationMenu(menu);
+
+    // tray = new Tray(trayIcon);
+    // tray.setContextMenu(menu);
+
+    app.setAppUserModelId(app.name);
+
+    // const splash = createSplashWindow();
+    const mainApp = createWindow();
+
+    mainApp.once('ready-to-show', () => {
+      // splash.destroy();
+      // mainApp.show();
+      setTimeout(() => {
+        // splash.destroy();
+        mainApp.show();
+      }, 2000)
+    })
+  });
+
+ipcMain.on('notify', (_, message) => {
+  new Notification({ title: 'Notification', body: message }).show();
+})
+
+ipcMain.on('app-quit', () => {
+  app.quit();
+})
+
+
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+})
